f3fc3847e2081f152d5d2594453ade47d946910e
D7MeKz <hwanseo6417@naver.com>
2023-03-23 16:34:42 +0900

    상품 등록 json parsing

--- server/app.py
+++ server/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_restx import Api
+
+from resources.product import * 
+
 # Custom 
-
 app = Flask(__name__)
-api = Api(app)
 
+
+api = Api(app)
+api.add_resource(MakeProduct, '/product')
 if __name__ =="__main__":
     app.run(debug=True, host='0.0.0.0')
---
+++ server/resources/product.py
@@ -0,0 +1,32 @@
+import os
+import json 
+import datetime 
+
+from flask import request,Response, jsonify
+from flask_restx import Resource
+
+class Product(Resource):
+    def get(self, product_id):
+        pass
+    
+    def patch(self,product_id):
+        pass
+    
+    def delete(self, product_id):
+        pass
+
+class MakeProduct(Resource):
+    def post(self):
+        result = {}
+        body = request.get_json()
+        product_obj = json.loads(json.dumps(body))
+        product_obj['created_date'] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+        product_obj['status'] = False
+       
+       # TODO : Add Query and Save to DB 
+       
+       return jsonify(result)
+        
+        
+        
+        
