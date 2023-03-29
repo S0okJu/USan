4bab45eefda3e970ed4fc36dff5d453dbd9f0648
D7MeKz <hwanseo6417@naver.com>
2023-03-29 21:57:59 +0900

    upload test

--- server/resources/product.py
+++ server/resources/product.py
@@ -83,11 +83,11 @@ def post_product():
         # 이미지 저장 및 DB 저장 
         img = obj['imgs']
         img_bytes =base64.b64decode(img)
-        img_data = binascii.unhexlify(img_bytes)
         filename = str(uuid.uuid4())
-        # total_path = UPLOAD_FOLDER+filename+".png"
+        total_path = UPLOAD_FOLDER+filename+".png"
         
-        img_data.save(UPLOAD_FOLDER,filename+".png")
+        with open(total_path,'wb') as im:
+            im.write(img_bytes)
         
         return {"status_code" : 200, "message":"Post product completely!"}
     except sqlalchemy.exc.SQLAlchemyError as e:
