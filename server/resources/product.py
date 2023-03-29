010ab339cffd180a5d621898c8b0f371fd25b752
D7MeKz <hwanseo6417@naver.com>
2023-03-29 18:21:48 +0900

    upload test

--- server/resources/product.py
+++ server/resources/product.py
@@ -18,7 +18,7 @@ from db.init_db import rdb
 import utils.color as msg
 from utils.changer import res_msg, model2json
 
-PROJECT_HOME = os.path.dirname(os.path.abspath(__file__))
+PROJECT_HOME = '/workspace/firstContainer/USan'
 UPLOAD_FOLDER = '{}/uploads/'.format(PROJECT_HOME)
 IMG_FORMATS = {'JPEG':'.jpg', 'PNG':'.png'}
 
