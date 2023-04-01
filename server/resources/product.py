36e970412d9c51f839123fc70b1a4bdcdd18bf8d
D7MeKz <hwanseo6417@naver.com>
2023-04-01 11:42:31 +0900

    사용자 페이징

--- server/resources/product.py
+++ server/resources/product.py
@@ -80,7 +80,7 @@ def display_product():
             print(result_json)
             
         return Response(
-            response = result_json,
+            response = jsonify(result_json),
             status=200,
             mimetype="application/json"
         )
