0a97577aacc218622428504521e4c0b5521b374e
D7MeKz <hwanseo6417@naver.com>
2023-04-07 21:08:31 +0900

    init_jwt 수정

--- server/jwt/init_jwt.py
+++ server/jwt/init_jwt.py
@@ -1,6 +1,8 @@
-from flask_jwt import JWT
+from flask_jwt import JWT, jwt_required, current_identity
+from .utility import authenticate, identity
 
+SECRET_KEY = 'usan'
 jwt = JWT()
 def init_jwt(app):
     global jwt 
-    jwt.init_app(app)
+    jwt.init_app(app,authenticate, identity)
