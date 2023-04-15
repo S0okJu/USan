41ab1d3716c2765721f7ede2723f6f38979ac400
D7MeKz <hwanseo6417@naver.com>
2023-04-15 23:01:06 +0900

    [+] 일부 수정

--- server/models.py
+++ server/models.py
@@ -41,7 +41,7 @@ class ProductModel(rdb.Model):
     modified_date = rdb.Column(rdb.DateTime(), nullable=False)
     favorite = rdb.Column(rdb.Boolean, nullable=True, server_default='0')
     status = rdb.Column(rdb.Boolean, nullable=False)
-    author_id = rdb.Column(rdb.Integer, rdb.ForeignKey('User.user_id'), nullable=True, server_default='0')
+    author_id = rdb.Column(rdb.Integer, rdb.ForeignKey('User.user_id'), nullable=False)
     product_imgs = rdb.relationship('ProductImageModel', backref=rdb.backref('product'), order_by='ProductImageModel.img_id')
 
     def to_dict(self):
