40f8ced47f487b325daa7d17f551077ea3bea00a
codefordmr01 <codefordmr01@naver.com>
2023-04-27 14:47:14 +0900

    change port num

--- app/src/main/java/com/example/usan_comb1/RetrofitClient.java
+++ app/src/main/java/com/example/usan_comb1/RetrofitClient.java
@@ -7,7 +7,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class RetrofitClient {
 
-    private static final String BASE_URL = "http://43.201.95.57:53132/";
+    private static final String BASE_URL = "http://43.201.95.57:53131/";
 
     private static Retrofit retrofit = null;
 
