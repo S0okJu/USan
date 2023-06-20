7166c5ade382fc5ef57173db035ed55d24712ba3
noweaqc <chaevly0818@naver.com>
2023-06-21 00:34:49 +0900

    진짜로 완성

--- xhdgkq/USan-usan_ch/app/src/main/java/com/example/usan_comb1/activity/product/AuthorSellProductDetailActivity.java
+++ xhdgkq/USan-usan_ch/app/src/main/java/com/example/usan_comb1/activity/product/AuthorSellProductDetailActivity.java
@@ -1,5 +1,8 @@
 package com.example.usan_comb1.activity.product;
 
+import static com.example.usan_comb1.utilities.Constants.BUYER;
+import static com.example.usan_comb1.utilities.Constants.SELLER;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
@@ -10,7 +13,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,15 +25,33 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.usan_comb1.ProductService;
 import com.example.usan_comb1.R;
 import com.example.usan_comb1.RetrofitClient;
+import com.example.usan_comb1.chat.ChatActivity;
 import com.example.usan_comb1.activity.profile.OtherProfileActivity;
 import com.example.usan_comb1.adapter.CardAdapter;
+import com.example.usan_comb1.models.Users;
 import com.example.usan_comb1.response.PostResult;
 import com.example.usan_comb1.response.RetroProduct;
+import com.example.usan_comb1.utilities.Constants;
+import com.example.usan_comb1.utilities.PreferenceManager;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import okhttp3.ResponseBody;
@@ -38,50 +59,40 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-// 상세페이지 내의 작성자 판매 물품
+// 상세페이지
 public class AuthorSellProductDetailActivity extends AppCompatActivity {
 
-    private String mProductName;
-    private int mProductPrice;
-    private TextView tvTitle, price, tvDetail, tvAuthor;
-    private ImageButton imgButton;
-    private ImageView profile;
-    private boolean isFavorite = false;
-    private ViewPager viewPager;
+    private TextView tvTitle, tvDetail, tvAuthor;
+    private TextView price;
+    private ImageView profile, productView;
+    private static ProductService mProductService;
+    public boolean isFavorite;
+    private static Integer productId;
     private String username;
-    private RecyclerView recyclerView;
-    private CardAdapter cardadapter;
-    private String accessToken;
-    private String author;
-
-    private ProductService mProductService;
-    private Integer productId;
-    private int[] imageReslds = {R.drawable.uploadimg, R.drawable.uploadimg, R.drawable.uploadimg, R.drawable.uploadimg, R.drawable.uploadimg};
+    private static String accessToken;
     private static final String KEY_IS_FAVORITE = "is_favorite";
+    public String author;
+    public Integer role;
 
+    private CardAdapter cardadapter;
+    private Button chat;
+
+    String TAG = "FirebaseChat";
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_author_sell_product_detail);
-
-        if (savedInstanceState != null) {
-            mProductName = savedInstanceState.getString("productName");
-            mProductPrice = savedInstanceState.getInt("productPrice");
-        } else {
-            // savedInstanceState가 null이면 초기화
-            mProductName = "";
-            mProductPrice = 0;
-        }
+        setContentView(R.layout.activity_detail);
 
         tvTitle = findViewById(R.id.tv_title);
-        price = findViewById(R.id.tvprice);
         tvDetail = findViewById(R.id.tv_detail);
         tvAuthor = findViewById(R.id.nickname);
         profile = findViewById(R.id.profile);
+        chat = findViewById(R.id.btnchat); // 채팅 버튼
+        productView = findViewById(R.id.productView);
 
-        mProductService = RetrofitClient.getRetrofitInstance().create(ProductService.class);
+        mProductService = RetrofitClient.getProductService();
 
         productId = getIntent().getIntExtra("product_id", -1);
 
@@ -92,17 +103,48 @@ public class AuthorSellProductDetailActivity extends AppCompatActivity {
         username = intent.getStringExtra("username");
         if (intent != null) {
             if (productId != -1) {
-                checkData(accessToken, productId);
+                checkData(productId);
             }
         }
+
+        downloadProductImage(accessToken, productId, 1);
+
+        tvAuthor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent otherProfileIntent = new Intent(AuthorSellProductDetailActivity.this, OtherProfileActivity.class);
+                otherProfileIntent.putExtra("username", tvAuthor.getText().toString());
+                startActivity(otherProfileIntent);
+            }
+        });
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent otherProfileIntent = new Intent(AuthorSellProductDetailActivity.this, OtherProfileActivity.class);
+                otherProfileIntent.putExtra("username", tvAuthor.getText().toString());
+                startActivity(otherProfileIntent);
+            }
+        });
+
+        // 채팅 버튼 누르기
+        chat.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                createOrJoinChat(String.valueOf(productId));
+            }
+        });
+
+
+        //하단바 가격을 나타내는 뷰 객체
+
+        price = findViewById(R.id.txtvprice);
+        mProductService = RetrofitClient.getRetrofitInstance().create(ProductService.class);
+
 
         // 제목, 가격, 설명 설정
         tvTitle.setText("상품 제목");
         tvDetail.setText("상품 설명입니다.");
-
-        viewPager = findViewById(R.id.viewPager);
-        AuthorSellProductDetailActivity.ImagePagerAdapter adapter = new AuthorSellProductDetailActivity.ImagePagerAdapter(this, imageReslds);
-        viewPager.setAdapter(adapter);
 
 
         if (savedInstanceState != null) {
@@ -181,26 +223,8 @@ public class AuthorSellProductDetailActivity extends AppCompatActivity {
         });
     }
 
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString("productName", mProductName);
-        outState.putInt("productPrice", mProductPrice);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        mProductName = savedInstanceState.getString("productName");
-        mProductPrice = savedInstanceState.getInt("productPrice");
-    }
-
-    public void checkData(String accessToken, Integer productId) {
-        // 서버 응답이 올 때까지 텍스트 뷰들의 텍스트를 초기화합니다.
-        tvTitle.setText("");
-        tvDetail.setText("");
-        tvAuthor.setText("");
+    // 상세페이지로 데이터 불러오기
+    public void checkData(Integer productId) {
 
         mProductService.getProduct(accessToken, productId).enqueue(new Callback<PostResult>() {
             @Override
@@ -208,22 +232,24 @@ public class AuthorSellProductDetailActivity extends AppCompatActivity {
                 if (response.isSuccessful() && response.body() != null) {
                     PostResult product = response.body();
 
-                    // 가져온 상품 정보를 화면에 표시합니다.
                     tvTitle.setText(product.getPost_Title());
                     tvDetail.setText(product.getPost_Content());
                     tvAuthor.setText(product.getPost_Author());
-                    price.setText(product.getPost_Price());
-
+                    price.setText(product.getPost_Price()+"원");
+                    // tvAuthor 텍스트 설정 후에 호출
                     loadUserPosts(product.getPost_Author());
+
+                    ImageView favoriteButton = findViewById(R.id.imgbtn);
 
                     // get Author username
                     author = product.getPost_Author();
                     System.out.println(author);
+
+
                     downloadImage();
 
-                    ImageView favoriteButton = findViewById(R.id.imgbtn);
-
                     isFavorite = product.isFavorite();
+
                     if (product.isFavorite() == true) {
                         favoriteButton.setImageResource(R.drawable.select_ic_heart);
                     } else {
@@ -241,6 +267,7 @@ public class AuthorSellProductDetailActivity extends AppCompatActivity {
             }
         });
     }
+
 
     private void updateFavoriteButtonImage() {
         ImageView favoriteButton = findViewById(R.id.imgbtn);
@@ -277,45 +304,46 @@ public class AuthorSellProductDetailActivity extends AppCompatActivity {
         });
     }
 
-    private static class ImagePagerAdapter extends PagerAdapter {
+    // 채팅
+    private void createOrJoinChat(String productId){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DatabaseReference chatRef = FirebaseDatabase.getInstance().getReference("chats");
+        String chatId = "chat_"+productId;
+        Users seller = new Users();
 
-        private Context context;
-        private int[] imageResIds;
+        database.collection("users").whereEqualTo("username", author).get().addOnCompleteListener(task -> {
+            if(task.isSuccessful() && task.getResult() != null){
+                QuerySnapshot result = task.getResult();
+                QueryDocumentSnapshot document = (QueryDocumentSnapshot) result.getDocuments().get(0);
+                String receiverId = document.getId(); // receiver id
 
-        public ImagePagerAdapter(Context context, int[] imageResIds) {
-            this.context = context;
-            this.imageResIds = imageResIds;
-        }
+                seller.setId(receiverId);
+                seller.setName(author);
 
-        @Override
-        public int getCount() {
-            return imageResIds.length;
-        }
+                if(username.equals(author)){
+                    role = SELLER;
+                }else{
+                    role = BUYER;
+                }
 
-        @Override
-        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-            return view == object;
-        }
+                Intent intent = new Intent(AuthorSellProductDetailActivity.this, ChatActivity.class);
+                intent.putExtra("chatId",chatId);
+                intent.putExtra("user", seller);
+                intent.putExtra("role",role); // TODO 본인의 역할을 넘겨줍니다.
+                intent.putExtra("prevInfo","detail");
+                startActivity(intent);
 
-        @NonNull
-        @Override
-        public Object instantiateItem(@NonNull ViewGroup container, int position) {
-            ImageView imageView = new ImageView(context);
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setImageResource(imageResIds[position]);
-            container.addView(imageView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-            return imageView;
-        }
+            }else{
+                System.out.println("Nop");
+            }
+        });
 
-        @Override
-        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-            container.removeView((View) object);
-        }
     }
 
     private void generateDataList(List<RetroProduct> productList) {
         cardadapter = new CardAdapter(this, productList);
     }
+
 
     // 프로필 이미지 다운로드
     private void downloadImage() {
@@ -351,6 +379,59 @@ public class AuthorSellProductDetailActivity extends AppCompatActivity {
                 // 이미지 다운로드 중 오류가 발생한 경우 기본 이미지를 설정합니다.
                 profile.setImageResource(R.drawable.ic_default_profile);
                 Toast.makeText(AuthorSellProductDetailActivity.this, "다운로드 오류", Toast.LENGTH_SHORT).show();
+                Log.e("Download error", "Download failed: " + t.getMessage());
+            }
+        });
+    }
+
+    // 이미지 다운로드
+    private void downloadProductImage(String accessToken, int productId, int num) {
+        Call<ResponseBody> call = mProductService.downloadImage(accessToken, productId, num);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    ResponseBody responseBody = response.body();
+                    if (responseBody != null) {
+                        try {
+                            String jsonString = responseBody.string();
+                            System.out.println(jsonString);
+                            JSONObject jsonObject = new JSONObject(jsonString);
+                            System.out.println(jsonObject);
+                            JSONArray imageUrls = jsonObject.getJSONArray("imgs");
+
+                            if (imageUrls != null && imageUrls.length() > 0) {
+                                String imageUrl = Constants.BASE_URL + imageUrls.getString(0);// 첫 번째 이미지 URL 가져오기
+                                System.out.println(imageUrl);
+                                Glide.with(AuthorSellProductDetailActivity.this)
+                                        .load(imageUrl)
+                                        .into(productView);
+                            } else {
+                                // 이미지 데이터가 없는 경우 기본 이미지를 설정합니다.
+                                productView.setImageResource(R.drawable.img_error);
+                                Log.e("Download error", "Download failed: No image URLs available");
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        // 이미지 데이터가 없는 경우 기본 이미지를 설정합니다.
+                        productView.setImageResource(R.drawable.img_error);
+                        Log.e("Download error", "Download failed: " + response.message());
+                    }
+                } else {
+                    // 서버 응답이 실패인 경우 기본 이미지를 설정합니다.
+                    productView.setImageResource(R.drawable.img_error);
+                    Log.e("Download error", "Download failed: " + response.message());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                // 이미지 다운로드 중 오류가 발생한 경우 기본 이미지를 설정합니다.
+                productView.setImageResource(R.drawable.img_error);
                 Log.e("Download error", "Download failed: " + t.getMessage());
             }
         });
